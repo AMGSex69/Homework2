f52028fa1f80ff20910fd36c5bc179103e5dba52
Egor <egordolgih@mail.ru>
2022-06-30 22:13:09 +0300

    add comments

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите число ");
+﻿//Вообще не уверен, что это законно, но я наконец-то узнал как щелкать такие задачки:))
+Console.Write("Введите число ");
 int number = Convert.ToInt32(Console.ReadLine());
 string numberTXT = Convert.ToString(number);
 if (numberTXT.Length > 2)
