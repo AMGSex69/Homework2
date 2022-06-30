d117edbd9256ad430ecda55b680adf6cee2ae443
Egor <egordolgih@mail.ru>
2022-06-30 21:50:59 +0300

    Task 2 done

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberTXT = Convert.ToString(number);
+if (numberTXT.Length > 2)
+{
+	Console.WriteLine("Третья цифра: " + numberTXT[2]);
+}
+else
+{
+	Console.WriteLine("Простите, тертьего числа не существует!");
+}
