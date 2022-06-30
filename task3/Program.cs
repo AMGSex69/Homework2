6a82a1dbd23e6c73be8a82ed0478da002364fa63
Egor <egordolgih@mail.ru>
2022-06-30 22:13:57 +0300

    task 3 done

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Решил попробовать сделать с методом, но и без него отлично получается) 
+void weekndDay()
+{
+	Console.Write("Введите число дня недели: ");
+	int number = Convert.ToInt32(Console.ReadLine());
+	if (number == 6 || number == 7)
+	{
+		Console.WriteLine("Ура, СЕГОДНЯ ВЫХОДНОЙ!");
+	}
+	else if (number < 0 || number > 7)
+	{
+		Console.WriteLine("Такого дня недели нет!");
+	}
+	else
+	{
+		Console.WriteLine("К сожалению, сегодня не выходной день!");
+	}
+}
+weekndDay();
